67afaffe1e19deec4783df84bea0f5203271d95e
Ian Cannon <ian.cannon@protonmail.com>
2021-11-05 11:13:55 -0700

    Create space object and photo models

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,27 @@
+from django.urls import reverse
 from django.db import models
+from datetime import date
 from django.contrib.auth.models import User
 
 # Create your models here.
+class SpaceObject(models.Model):
+		name = models.CharField(max_length=50)
+		so_type = models.CharField(max_length=50)
+		mass = models.CharField(max_length=50)
+		diameter = models.CharField(max_length=50)
+		distance = models.CharField(max_length=50)
+		description = models.CharField(max_length=150)
+		user = models.ForeignKey(User, on_delete=models.CASCADE)
+		# missions = models.ManyToManyField(Missions) #ADD LATER ALONG W MODEL
+  # users = models.ManyToManyField(Users) #ADD LATER TOO
+		def __str__(self):
+			return f'({self.id}) - {self.name}'
+		def get_absolute_url(self):
+				return reverse('detail', kwargs={'spaceobject_id': self.id})
+
+class Photo(models.Model):
+		url=models.CharField(max_length=200)
+		ship=models.ForeignKey(SpaceObject, on_delete=models.CASCADE)
+
+		def __str__(self):
+				return f"Photo for spaceobject_id:{self.spaceobject_id}@{self.url}"
