84bc7afb421c138c933d4d6e99d119518525ccc8
Ian Cannon <ian.cannon@protonmail.com>
2021-11-05 10:46:44 -0700

    Add authentication and authorization and login and logout

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import HttpResponse
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+import uuid
+import os
+import boto3 
 
 # Create your views here.
 def home(request):
@@ -8,3 +14,17 @@ def home(request):
 
 def about(request):
 		return render(request, 'about.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
