93f04f920331681a1d0bf3dadd8b896df338d17c
Jesus <Jesuspineda9512@gmail.com>
2021-11-05 14:25:44 -0700

    Planet functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,12 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-		path('', views.home, name='home'),
-		path('about/', views.about, name='about'),
-		path('accounts/signup/', views.signup, name='signup'),
-		path('stars/', views.stars_index, name='index'),
-		path('stars/create/', views.StarCreate.as_view(), name='stars_create'),
-  path('stars/<int:star_id>/', views.stars_detail, name='detail'),
- path('stars/<int:pk>/update/', views.StarUpdate.as_view(), name='stars_update'),
-  path('stars/<int:pk>/delete/', views.StarDelete.as_view(), name='stars_delete'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('stars/', views.stars_index, name='index'),
+    path('stars/create/', views.StarCreate.as_view(), name='stars_create'),
+    path('stars/<int:star_id>/', views.stars_detail, name='detail'),
+    path('stars/<int:pk>/update/', views.StarUpdate.as_view(), name='stars_update'),
+    path('stars/<int:pk>/delete/', views.StarDelete.as_view(), name='stars_delete'),
+
+    path('planets/', views.planets_index, name='planets_index'),
+    path('planets/create/', views.PlanetCreate.as_view(), name='planets_create'),
+    path('planets/<int:planet_id>/', views.planets_detail, name='planets_detail'),
+    path('planets/<int:pk>/update/', views.PlanetUpdate.as_view(), name='planets_update'),
+    path('planets/<int:pk>/delete/', views.PlanetDelete.as_view(), name='planets_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,53 +7,102 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 import uuid
 import os
-import boto3 
-from .models import Star, Photo
+import boto3
+from .models import Star, Planet
 
 # Create your views here.
+
+
 def home(request):
-		return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def about(request):
-		return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
 
 @login_required
 def stars_index(request):
-				stars = Star.objects.all()
-				return render(request, 'stars/index.html', {'stars': stars})
+    stars = Star.objects.all()
+    return render(request, 'stars/index.html', {'stars': stars})
+
 
 @login_required
 def stars_detail(request, star_id):
-  star = Star.objects.get(id=star_id)
-  return render(request, 'stars/detail.html', {
-    'star': star,
-  })
+    star = Star.objects.get(id=star_id)
+    return render(request, 'stars/detail.html', {
+        'star': star,
+    })
+
 
 class StarCreate(LoginRequiredMixin, CreateView):
-		model = Star
-		fields = ['name', 'star_type', 'mass', 'diameter', 'distance','description']
-		def form_valid(self, form):
-				form.instance.user=self.request.user
-				return super().form_valid(form)
+    model = Star
+    fields = ['name', 'star_type', 'mass',
+              'diameter', 'distance', 'description']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
 
 class StarUpdate(LoginRequiredMixin, UpdateView):
-		model = Star
-		fields = ['star_type', 'mass', 'diameter', 'description']
+    model = Star
+    fields = ['star_type', 'mass', 'diameter', 'description']
+
 
 class StarDelete(LoginRequiredMixin, DeleteView):
-		model = Star
-		success_url = '/stars/'
+    model = Star
+    success_url = '/stars/'
+
+
+
+
+
+
+
+@login_required
+def planets_index(request):
+    planets = Planet.objects.all()
+    return render(request, 'planets/index.html', {'planets': planets})
+
+@login_required
+def planets_detail(request, planet_id):
+    planet = Planet.objects.get(id=planet_id)
+    return render(request, 'planets/detail.html', {
+        'planet': planet,
+    })
+
+
+class PlanetCreate(LoginRequiredMixin, CreateView):
+    model = Planet
+    fields = ['name', 'planet_type', 'mass',
+              'diameter', 'distance', 'description']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class PlanetUpdate(LoginRequiredMixin, UpdateView):
+    model = Planet
+    fields = ['planet_type', 'mass', 'diameter', 'description']
+
+
+class PlanetDelete(LoginRequiredMixin, DeleteView):
+    model = Planet
+    success_url = '/planets/'
