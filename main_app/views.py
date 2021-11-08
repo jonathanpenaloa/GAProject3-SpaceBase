59fd33df4e2f849c52b74e6982b234b60dbe095e
Zsander <zsander@Infomorph.localdomain>
2021-11-08 09:53:59 -0800

    Update HTML

--- main_app/views.py
+++ main_app/views.py
@@ -136,7 +136,7 @@ class SatelliteDelete(LoginRequiredMixin, DeleteView):
     model = Satellite
 
     success_url = '/satellites/'
-=======
+
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
